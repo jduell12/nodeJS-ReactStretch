a79c819bfb6fea23d70d4f279043e8cf55451cb5
Jessica <duellje@oregonstate.edu>
2020-08-07 13:16:15 -0400

    can delete project

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,7 +1,18 @@
 import React from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 const ProjectCard = (props) => {
   const { id, name, description, completed } = props.project;
+  const history = useHistory();
+
+  const deleteProject = () => {
+    axios
+      .delete(`http://localhost:8000/projects/${id}`)
+      .then((res) => console.log("Successfully deleted"))
+      .catch((err) => console.log(err))
+      .finally(history.push("/"));
+  };
 
   return (
     <div>
@@ -9,6 +20,8 @@ const ProjectCard = (props) => {
       <h3>Project #{id} </h3>
       <p>Description: {description}</p>
       {completed ? <p>Completed: Yes</p> : <p>Completed: No</p>}
+      <button>Edit Project</button>
+      <button onClick={() => deleteProject()}>Delete Project</button>
     </div>
   );
 };
