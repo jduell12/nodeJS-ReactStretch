c21bd4a28c955dae45dea0566e131c699ecd4e8a
Jessica <duellje@oregonstate.edu>
2020-08-07 12:40:56 -0400

    set up application to get projects from api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Switch, Route, useHistory, Link } from "react-router-dom";
 
-function App () {
-  return <div className='App'></div>
+//context
+import { ProjectContext } from "./context/ProjectContext";
+
+//components
+import Projects from "./components/Projects";
+
+function App() {
+  const history = useHistory();
+
+  const [projectInfo, setProjects] = useState({
+    id: "",
+    name: "",
+    description: "",
+    completed: false,
+  });
+
+  return (
+    <div className="App">
+      <Switch>
+        <ProjectContext.Provider value={{ projectInfo, setProjects }}>
+          <Route exact path="/projects">
+            <Projects />
+          </Route>
+          <Route exact path="/">
+            <div>
+              <h1>Get Projects</h1>
+              <button onClick={() => history.push("/projects")}>
+                Display Projects
+              </button>
+            </div>
+          </Route>
+        </ProjectContext.Provider>
+      </Switch>
+      <Link to="/">Home</Link>
+    </div>
+  );
 }
 
-export default App
+export default App;
